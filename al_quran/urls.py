8232d9e51bebad28d11a7cd98196e2536f54e293
AzeezBello <azeez@scholarx.co>
2020-05-19 14:17:23 +0100

    quran set up

--- al_quran/urls.py
+++ al_quran/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-]
+    path('', include('quran.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
